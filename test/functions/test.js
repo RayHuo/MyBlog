c7ae7e37609981f1343106c029602080bf81c5cb
RayHuo <rayhuo.81@gmail.com>
2014-09-01 16:17:05 +0800

    modify Article

--- test/functions/test.js
+++ test/functions/test.js
@@ -2,8 +2,38 @@ function Article() {
 	this.title = "N/A";
 	this.author = "N/A";
 	this.publishTime = "N/A";
+	this.content = "";
 }
 
 Article.propotype.getTitle = function() {
-	return title;
+	return this.title;
 };
+
+Article.propotype.setTitle = function(title) {
+	this.title = title;
+};
+
+Article.propotype.getAuthor = function() {
+	return this.author;
+};
+
+Article.propotype.setAuthor = function(author) {
+	this.author = author;
+};
+
+Article.propotype.getPublishTime = function() {
+	return this.publishTime;
+};
+
+Article.propotype.setPublishTime = function(publishTime) {
+	this.publishTime = publishTime;
+};
+
+Article.propotype.addContent = function(content) {
+	this.content += content;
+};
+
+Article.propotype.getContent = function() {
+	return this.content;
+};
+
