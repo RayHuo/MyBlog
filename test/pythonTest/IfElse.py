827034cdb3ba72c8ae4dff74dfa40bc9c969f2bc
RayHuo <rayhuo.81@gmail.com>
2014-09-10 16:59:30 +0800

    some new test

---
+++ test/pythonTest/IfElse.py
@@ -0,0 +1,13 @@
+a = input("First : ")
+b = input("Second : ")
+
+# if-elif-else
+# remeber the ":" at the end of the condition
+if a == b :
+    print "first is equal to second!"
+elif a < b :
+    print "first is less than second!"
+else :
+    print "first is larger than second!"
+
+# python has no switch
