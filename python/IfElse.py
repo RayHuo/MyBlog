1edd3afa5550d6ccbbe1c9350540b77ce13a2139
RayHuo <rayhuo_81@hotmail.com>
2014-09-11 15:01:06 +0800

    and or not

--- python/IfElse.py
+++ python/IfElse.py
@@ -11,3 +11,14 @@ else :
     print "first is larger than second!"
 
 # python has no switch
+
+
+if a + b < 10 and a * b < 20 : 
+    print "%d + %d < 10, and %d * %d < 20" % (a, b, a, b)
+elif a + b < 10 or a * b < 20 :
+    if not (a + b < 10) : 
+        print "%d * %d < 20" % (a, b)
+    else : 
+        print "%d + %d < 10" % (a, b)
+else :
+    print "Nothing"
