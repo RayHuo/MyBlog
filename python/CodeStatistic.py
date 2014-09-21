9ff411a13b098172a0aa1d679e984d612537cb0d
RayHuo <rayhuo_81@hotmail.com>
2014-09-21 15:03:42 +0800

    new list

--- python/CodeStatistic.py
+++ python/CodeStatistic.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+import sys
 
 direct = "./fileTest/" # only can use relative path, not absolute path
 
@@ -7,4 +8,15 @@ print "Files under direct : ", direct
 
 for parent, dirnames, filenames in os.walk(direct) :
     for filename in filenames :
+        # os.path.splitext() will split the filename into two part : filename and extendname, "abc.txt" : "abc" and ".txt"
+        if os.path.splitext(filename)[1] == '.py' :  # only output the file end with '.cpp'
             print filename  
+            # f = open(filename)
+print ""
+
+for f in os.listdir(direct) : 
+    print f[:-2]
+    if f[:-2] == '.py' : 
+        file_ = open(direct + f)
+        print "open successfully!"
+        file_.close()
